56e1d615834a2cdf9c0185c82f7595f442ea9ffa
Șerban Robu <serbanrobu@protonmail.com>
2023-03-08 18:15:13 +0200

    Initialize project

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bacon # Background rust code check
+            cargo-audit
+            cargo-expand
+            cargo-tarpaulin # Code coverage reporting tool
+            cargo-watch
+            clang
+            evcxr # Rust REPL
+            llvmPackages.bintools
+            nil # Nix language server
+            nixpkgs-fmt
+            (rust-bin.selectLatestNightlyWith (toolchain:
+              toolchain.default.override {
+                extensions = [ "rust-analyzer" "rust-src" ];
+                targets = [ "wasm32-unknown-unknown" ];
+              }))
+            taplo # Toml language server
+          ];
+        };
+      }
+    );
+}
